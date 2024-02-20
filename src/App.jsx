52a2795e2dab997a272f3296227904d29dc29fb7
aidyn <aidyn.meiramkhanulu@gmail.com>
2024-02-20 19:32:22 +0600

    fe:added main

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import "./App.css";
-import Login from "./Login";
-import Signin from "./Signin";
+import './App.css';
+import Login from './Login';
+import { Main } from './component/Main';
+import Signin from './Signin';
 
 function App() {
-  return (
-    <div className="text-white h-[100vh] flex items-center justify-center bg-cover">
-      <div className="bg-[url('./assets/hero-bg.jpg')]"></div>
-      <Routes>
-        <Route path='/signin' element={ <Signin />} />
-        <Route path='/login' element={ <Login  />} />  
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='text-white h-[100vh] flex items-center justify-center bg-cover'>
+			<div className="bg-[url('./assets/hero-bg.jpg')]"></div>
+			<Routes>
+				<Route path='/signin' element={<Signin />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/main' element={<Main />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
